bce78f6ae1389936583f81228eb44cdf87402fb1
KE.YANG <ke.yang@outlook.com>
2023-08-03 01:05:24 +0800

    fix(weui): input

--- packages/weui-vue/src/components/form/input.tsx
+++ packages/weui-vue/src/components/form/input.tsx
@@ -3,11 +3,11 @@ import { defineComponent, ref, watchEffect } from 'vue'
 export const Input = defineComponent({
   inheritAttrs: false,
   props: {
-    modelValue: String
+    modelValue: [String, Number]
   },
   emits: ['update:modelValue'],
   setup(props, { emit, attrs }) {
-    const modelValue = ref<string>(props.modelValue || '')
+    const modelValue = ref<string | number>(props.modelValue || '')
 
     watchEffect(() => {
       emit('update:modelValue', modelValue.value)
